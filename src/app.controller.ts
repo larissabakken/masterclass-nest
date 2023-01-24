6333cd9ab7fffc66603da3a3830bb79ef17a2349
Larissa Ramos <devlarissasr@gmail.com>
2023-01-24 11:23:34 +0100

    feat add prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const member = await this.prisma.members.create({
+      data: {
+        id: "teste",
+        name: 'John Doe',
+        function: 'CEO',
+    },
+  });
+    return member;
   }
 }
